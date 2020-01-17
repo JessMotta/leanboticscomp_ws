4723e017c8243706fafac07d7f0b200c5b5bf84f
JessMotta <jessicalimamotta@gmail.com>
2020-01-17 14:35:19 -0300

    robot with lidar vlp16 and plugin

---
+++ learnboticscomp_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/jess/learnboticscomp_ws/src
+# Build directory: /home/jess/learnboticscomp_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_learnbotics")
+subdirs("robot_learnbotics")
---
+++ learnboticscomp_ws/build/gazebo_learnbotics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jess/learnboticscomp_ws/src/gazebo_learnbotics
+# Build directory: /home/jess/learnboticscomp_ws/build/gazebo_learnbotics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
